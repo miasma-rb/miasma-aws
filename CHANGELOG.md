b1e5f3ce0db6d890c2ab83ead3c3c7460b14d30d
Chris Roberts <chrisroberts.code@gmail.com>
2015-01-11 06:50:06 -0800

    Update for release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+## v0.1.2
+* Migrate spec coverage
+* Update storage behavior to use streamable helper
+
 ## v0.1.0
 * Initial release
--- lib/miasma-aws/version.rb
+++ lib/miasma-aws/version.rb
@@ -1,4 +1,4 @@
 module MiasmaAws
   # Current library version
-  VERSION = Gem::Version.new('0.1.1')
+  VERSION = Gem::Version.new('0.1.2')
 end
