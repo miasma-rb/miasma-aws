4f1a89835e21a056654e04c9f374ff53d770c192
Chris Roberts <code@chrisroberts.org>
2016-02-17 14:52:02 -0800

    Include STS session token usage within spec

--- test/specs/aws_sts_spec.rb
+++ test/specs/aws_sts_spec.rb
@@ -73,4 +73,27 @@ describe Miasma::Models::Orchestration::Aws do
 
   end
 
+  describe 'MFA session token provided' do
+
+    describe 'MFA root credentials to STS assume role', :vcr do
+
+      it 'should properly assume role and access the API' do
+        orchestration = Miasma.api(
+          :type => :orchestration,
+          :provider => :aws,
+          :credentials => {
+            :aws_access_key_id => ENV.fetch('MIASMA_AWS_ACCESS_KEY_ID_MFA', 'test-key-mfa'),
+            :aws_secret_access_key => ENV.fetch('MIASMA_AWS_SECRET_ACCESS_KEY_MFA', 'test-key-mfa'),
+            :aws_region => ENV.fetch('MIASMA_AWS_REGION', 'us-west-1'),
+            :aws_sts_session_token_code => ENV.fetch('MIASMA_AWS_STS_SESSION_TOKEN_CODE', 'test-role-token'),
+            :aws_sts_role_arn => ENV.fetch('MIASMA_AWS_STS_ROLE_ARN_MFA', 'test-role-arn-mfa')
+          }
+        )
+        orchestration.stacks.all.must_be_kind_of Array
+      end
+
+    end
+
+  end
+
 end
